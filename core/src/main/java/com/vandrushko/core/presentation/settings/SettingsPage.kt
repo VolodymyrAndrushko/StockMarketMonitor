6b0e44799df74ea8fe91a415dddda7f658984152
vandrushko <mrkamilot@gmail.com>
2025-08-31 03:22:25 +0300

    fixed SettingsScreen alignment

--- core/src/main/java/com/vandrushko/core/presentation/settings/SettingsPage.kt
+++ core/src/main/java/com/vandrushko/core/presentation/settings/SettingsPage.kt
@@ -12,8 +12,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.vandrushko.core.R
@@ -45,11 +47,13 @@ fun SettingsScreen(
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(vertical = 8.dp),
-                horizontalArrangement = Arrangement.SpaceBetween
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
             ) {
                 Text(
                     text = stringResource(R.string.update_every, period.seconds),
-                    style = MaterialTheme.typography.bodyLarge
+                    style = MaterialTheme.typography.bodyLarge,
+                    textAlign = TextAlign.Center
                 )
 
                 Checkbox(
